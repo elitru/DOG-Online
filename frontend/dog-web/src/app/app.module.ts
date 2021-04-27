5fd4ea6238e318e237398143f7a50cfc7c368562
Elias Trummer <trummer-linhard@gmx.at>
2021-04-27 09:30:21 +0200

    basic setup

---
+++ frontend/dog-web/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateLobbyComponent } from './modules/pages/create-lobby/create-lobby.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateLobbyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
